802970a79c7b04452e1eb8b882ec5926580de070
AlbanyOrc9 <x>
2018-05-20 20:18:49 -0400

    Exercise 0-4

---
+++ zero/zero_four/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "// a small C++ program\n";
+    std::cout << "#include <iostream>\n\n";
+    std::cout << "int main()\n{\n";
+    std::cout << "\tstd::cout << \"Hello, world!\" << std::endl;\n";
+    std::cout << "\treturn 0;\n}" << std::endl;
+
+    return 0;
+}
